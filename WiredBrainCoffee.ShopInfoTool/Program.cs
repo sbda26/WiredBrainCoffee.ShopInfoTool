e7064ddb2df6d475532b6fb45569faafe1c33a53
sbda26 <46138243+sbda26@users.noreply.github.com>
2020-08-19 11:57:18 -0400

    Implement help command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using WiredBrainCoffee.DataAccess;
+using WiredBrainCoffee.DataAccess.Model;
 
 namespace WiredBrainCoffee.ShopInfoTool
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+
+            Console.WriteLine("Write 'help' to list available commands");
+
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                IEnumerable<CoffeeShop> coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee shop commands:");
+                    foreach (CoffeeShop coffeeShop in coffeeShops)
+                        Console.WriteLine($"> {coffeeShop.Location}");
+                }
+
+            }
+        }
     }
-  }
 }
