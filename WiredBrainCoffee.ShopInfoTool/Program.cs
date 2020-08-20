160a226506d892b0e25bf289bb9bc92514b22199
sbda26 <46138243+sbda26@users.noreply.github.com>
2020-08-20 14:16:10 -0400

    Handle coffee shop commands

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using WiredBrainCoffee.DataAccess;
 using WiredBrainCoffee.DataAccess.Model;
@@ -32,6 +33,27 @@ namespace WiredBrainCoffee.ShopInfoTool
                         Console.WriteLine("> Available coffee shop commands:");
                         foreach (CoffeeShop coffeeShop in coffeeShops)
                             Console.WriteLine($"> {coffeeShop.Location}");
+                    }
+                    else
+                    {
+                        List<CoffeeShop> foundCoffeeShops = coffeeShops
+                            .Where(x => x.Location.StartsWith(line, StringComparison.OrdinalIgnoreCase))
+                            .ToList();
+
+                        if (foundCoffeeShops.Count == 0)
+                            Console.WriteLine($"> Command '{line}' not found");
+                        else if (foundCoffeeShops.Count == 1)
+                        {
+                            var coffeeShop = foundCoffeeShops.Single();
+                            Console.WriteLine($"> Location: {coffeeShop.Location}");
+                            Console.WriteLine($"> Beans in stock: {coffeeShop.BeansInStockInKg} kg");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"> Multiple matching coffee shop commands found:");
+                            foreach (CoffeeShop coffeeType in foundCoffeeShops)
+                                Console.WriteLine($"> {coffeeType.Location}");
+                        }
                     }
                 }
             }
